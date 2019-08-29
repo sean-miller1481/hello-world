15592a87be6888ed21fd381abfdecd3f1e5e8e45
sean-miller1481 <54679830+sean-miller1481@users.noreply.github.com>
2019-08-29 11:24:48 -0500

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,18 @@
+/**
+* Author: Sean
+* Date: 2019/29/08
+*
+* A simple hello world program in C
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+  printf("Sean Miller\n");
+
+  printf("Electrical Engineering");
+
+  return 0;
+}
